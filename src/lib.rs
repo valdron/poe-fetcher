31c3152782f68fb77e52b45d7ea3e0955b1d5439
Paul Seehofer <seehofer.paul@gmail.com>
2017-06-11 13:50:19 +0200

    extracted functions from iterator impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 
 extern crate hyper;
+extern crate regex;
 
 use hyper::{Client, Url};
+use hyper::method::Method;
+use regex::Regex;
+use std::io::Read;
+
+#[derive(Debug)]
+struct PoeSite {
+    change_id: String,
+    body: String,
+}
 
 #[derive(Debug)]
 struct PoeFetcher {
     url: Url,
     client: Client,
+    next_id: String,
 }
 
 
@@ -15,15 +26,60 @@ impl PoeFetcher {
         Self {
             url: start_url,
             client: Client::new(),
+            next_id: "".into(),
         }
     }
 }
 
 impl Iterator for PoeFetcher {
-    type Item = String;
+    type Item = PoeSite;
     fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!();
+
+        self.url
+            .query_pairs_mut()
+            .clear()
+            .append_pair("id", &self.next_id);
+
+        let response = self.client.request(Method::Get, self.url.clone()).send();
+
+
+
+        match response {
+            Ok(mut res) => {
+
+                let mut result = String::with_capacity(5_000_000);
+                let old_id = self.next_id.clone();
+
+                let size = res.read_to_string(&mut result);
+
+                match size {
+                    Ok(x) if x == 0 => return None,
+                    Err(_) => return None,
+                    _ => {}
+                }
+
+                {
+                    
+                }
+
+                Some(PoeSite {
+                         change_id: old_id,
+                         body: result,
+                     })
+            }
+
+            _ => None,
+        }
     }
+}
+
+fn extract_next_id(s: &str) -> Result<String, String> {
+    let re = Regex::new("\\{\"next_change_id\":\"((?:\\d|-)+)\",").unwrap();
+    let mat = match re.captures(s).and_then(|cap| cap.get(1)) {
+        Some(x) => x,
+        None => return Err("no changeid found in body".into()),
+    };
+    Ok(mat.as_str().to_string().clone())
 }
 
 
